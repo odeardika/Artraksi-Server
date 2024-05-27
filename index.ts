2c09b9a8d29bec66d7287b9e0528b856e7124b1b
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2024-05-27 13:31:36 +0800

    feat : Typescript setup for express framework

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express ,{ Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res : Response) => {
+    res.send("<h1>Artraksi Server Running");
+})
+
+app.post("/auth/regis", (req : Request, res : Response) => {
+    
+})
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
