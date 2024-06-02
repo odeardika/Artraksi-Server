f9ed963b93eb7f023bd7d8c40c2ef440a0782ced
Ode Ardika <104220059+odeardika@users.noreply.github.com>
2024-06-02 08:57:53 +0800

    fix : Update the public directory path

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(authRoute);
 app.use(articlesRoute);
